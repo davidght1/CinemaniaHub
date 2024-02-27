3a0931b41a347fd5ab86b5b963ec18cd0d4af09f
David Agam <davidght1@gmail.com>
2024-02-27 09:43:04 +0200

    new file:   routes/movies.js

---
+++ routes/movies.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router()
+
+//get all movies
+
+//get single movie
+
+//update user vote about single movie
+
+//update user rating about single movie
+
+
+//add movie (only by admin)
+
+//edit movie (only by admin)
+
+//delete movie(only by admin)
+
+
+module.exports = router
