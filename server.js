600ed594dca6d628cc51f8357c737a75db079bdc
David Agam <davidght1@gmail.com>
2024-02-27 09:24:44 +0200

    modified:   server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log('server running')
+require('dotenv').config()
+const bodyParser = require('body-parser')
+const mongoose =require('mongoose')
+const express = require('express')
+const app = express()
+
+//Middlewares
+app.use(express.json())
+app.use(bodyParser.json())
+
+
+//Routes
+
+//Error Middleware
+
+
+//Connect to DB and start server
+const PORT = process.env.PORT || 5000;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`)
+      console.log('DB Connected Successfully');
+    });
+  })
+  .catch((err) => console.log(err));
